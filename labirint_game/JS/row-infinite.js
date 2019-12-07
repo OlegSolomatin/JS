6cedd8631faa5b632e2f7428423ba37648d7904b
OlegSolomatin <olegsolomatin2018@yandex.ru>
2019-12-07 15:44:37 +0300

    update labirint adde folder

--- labirint_game/JS/row-infinite.js
+++ labirint_game/JS/row-infinite.js
@@ -6,7 +6,7 @@ window.onkeydown = function move() {
     if  (event.keyCode===37) {
         x-=5;
         document.getElementById('row').style.left = x + 'px';
-        document.getElementById('row').style.transition = 0.5 + 's';
+        document.getElementById('row').style.transition = 0.2 + 's';
         c = document.getElementById('row').getBoundingClientRect();
         if  (c===finish) {
             alert("Happy NEW YEAR");
@@ -14,7 +14,7 @@ window.onkeydown = function move() {
     }   else if (event.keyCode===39) {
         x+=5;
         document.getElementById('row').style.left = x + 'px';
-        document.getElementById('row').style.transition = 0.5 + 's';
+        document.getElementById('row').style.transition = 0.2 + 's';
         c = document.getElementById('row').getBoundingClientRect();
         if  (c===finish) {
             alert("Happy NEW YEAR");
@@ -22,7 +22,7 @@ window.onkeydown = function move() {
     }   else if (event.keyCode===38) {
         y-=5;
         document.getElementById('row').style.top = y + 'px';
-        document.getElementById('row').style.transition = 0.5 + 's';
+        document.getElementById('row').style.transition = 0.2 + 's';
         c = document.getElementById('row').getBoundingClientRect();
         if  (c===finish) {
             alert("Happy NEW YEAR");
@@ -30,7 +30,7 @@ window.onkeydown = function move() {
     }   else if (event.keyCode===40) {
         y+=5;
         document.getElementById('row').style.top = y + 'px';
-        document.getElementById('row').style.transition = 0.5 + 's';
+        document.getElementById('row').style.transition = 0.2 + 's';
         c = document.getElementById('row').getBoundingClientRect();
         if  (c===finish) {
             alert("Happy NEW YEAR");
@@ -40,7 +40,7 @@ window.onkeydown = function move() {
         y-=5;
         document.getElementById('row').style.left = x + 'px';
         document.getElementById('row').style.top = y + 'px';
-        document.getElementById('row').style.transition = 0.5 + 's';
+        document.getElementById('row').style.transition = 0.2 + 's';
         c = document.getElementById('row').getBoundingClientRect();
         if  (c===finish) {
             alert("Happy NEW YEAR");
@@ -50,7 +50,7 @@ window.onkeydown = function move() {
         y-=5;
         document.getElementById('row').style.left = x + 'px';
         document.getElementById('row').style.top = y + 'px';
-        document.getElementById('row').style.transition = 0.5 + 's';
+        document.getElementById('row').style.transition = 0.2 + 's';
         c = document.getElementById('row').getBoundingClientRect();
         if  (c===finish) {
             alert("Happy NEW YEAR");
@@ -60,7 +60,7 @@ window.onkeydown = function move() {
         y+=5;
         document.getElementById('row').style.left = x + 'px';
         document.getElementById('row').style.top = y + 'px';
-        document.getElementById('row').style.transition = 0.5 + 's';
+        document.getElementById('row').style.transition = 0.2 + 's';
         c = document.getElementById('row').getBoundingClientRect();
         if  (c===finish) {
             alert("Happy NEW YEAR");
@@ -70,7 +70,7 @@ window.onkeydown = function move() {
         y+=5;
         document.getElementById('row').style.left = x + 'px';
         document.getElementById('row').style.top = y + 'px';
-        document.getElementById('row').style.transition = 0.5 + 's';
+        document.getElementById('row').style.transition = 0.2 + 's';
         c = document.getElementById('row').getBoundingClientRect();
         if  (c===finish) {
             alert("Happy NEW YEAR");
---
+++ learnJS/lesson1/js/jssc.js
@@ -0,0 +1,21 @@
+/*let tempC = prompt("");
+let tempF = alert( (9/5) * tempC + 32);
+
+let name = prompt("");
+let admin = name;
+alert(admin);*/
+
+/*let a = -1;
+let b = 10;
+
+a>0 && b>0 ? console.log(a-b) : a<0 && b<0 ? console.log(a*b) : a = Math.floor(Math.random() * 16) ;
+console.log(a);*/
+
+let i = '*';
+let n = 1;
+let arr = [];
+while (n<=20){
+    arr.push(i);
+    console.log(arr.join(' '));
+    n++;
+}
