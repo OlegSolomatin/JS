31ec96375370e25133d71e887db5fcd1356fff2d
OlegSolomatin <olegsolomatin2018@yandex.ru>
2019-12-16 15:03:47 +0300

    update form reg

--- labirint_game/index.php
+++ labirint_game/index.php
@@ -1,37 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <link href="https://fonts.googleapis.com/css?family=Mountains+of+Christmas&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <div class="forma-registration">
-        <form class="forma-reg-newuser">
-            <h1 class="title-form">Sign Up for Free</h1>
-            <div class="forma-reg-input-cont">
-                <label>
-                    <input class="input-field" name="login" type="text" placeholder="Login">
-                </label>
-                <label>
-                    <input class="input-field" name="pass" type="password" placeholder="Password">
-                </label>
-                <label>
-                    <input class="input-field" name="last-pass" type="password" placeholder="Password">
-                </label>
-                <label>
-                    <input class="input-field" name="email" type="email" placeholder="Your E'mail">
-                </label>
-                <label class="custom-redemption-container">
-                    <input class="checkbox" type="checkbox"><strong class="custom-redemption"><a>Пользовательское соглашение</a></strong>
-                </label>
-            </div>
-            <div class="but-form-reg">
-                <button></button>
-            </div>
-        </form>
-    </div>
+<?php
+    require_once("form-registr.php");
+    ?>
     <div class="snow"></div>
     <div class="snow"></div>
     <div class="snow"></div>
